2a50640520edc8b87a92c76fbb95a9f7a6fdaadb
bsilvereagle <bsilvereagle@gmail.com>
2016-08-16 17:39:30 -0400

    Added basic menu functionality

--- tidalbar.py
+++ tidalbar.py
@@ -3,6 +3,7 @@ import mpv
 import getpass
 import time
 from requests import HTTPError
+from collections import OrderedDict
 
 
 #
@@ -27,9 +28,8 @@ while login_attempts < allowed_attempts:
     try:
         username = input('TIDAL username: ')
         password = getpass.getpass('TIDAL password: ')
-        session.login(username,password)
-        if session.check_login():
-            print('Successfully logged in!')
+        if session.login(username, password):
+            print('\N{EIGHTH NOTE} Successfully logged in! \N{EIGHTH NOTE}')
             break
         else:
             print('Error establishing a session. Check your internet connection.')
@@ -41,10 +41,92 @@ if login_attempts == allowed_attempts:
     exit()
 
 #
-#   CAST THE CURSES
+#   MENUS
+#
+#   Menus are dictionaries where the key is the numeric selction
+#   identifier for the menu, and the item is a tuple containing the
+#   text to be displayed and the function to be executed for the
+#   given selection
+#
+#   sw_menu = {'1':('Han',han_shot_first),'2':('Greedo',greedo_shot_first)}
+#
+#   would display the following menu when called with run_menu(sw_menu)
+#
+#   1) Han
+#   2) Greedo
+#
+#   and execute either han_shot_first() or greedo_shot_first()
+#
+#   
+
+#
+#   MENU FUNCTIONS
 #
 
-playlist = session.get_user_playlists(session.user.id)
+def print_menu(menu_map):
+    # Sort the menu and store in an collections.OrderedDict
+    # to ensure that items are displayed 1-9
+    ordered_menu = OrderedDict(sorted(menu_map.items()))
+    
+    print('\n')
+    for item in ordered_menu:
+        print('\t%s)\t%s' % (item, ordered_menu[item][0]))
+    print('\n')
+
+def receive_menu(menu_map):
+    selection = input('Selection: ')
+    # Look up the selection in the dictionary
+    return(menu_map.get(selection))
+
+def run_menu(menu):
+    # Present menu and get user selection
+    result = None
+    while result is None:
+        print_menu(menu) 
+        # dict.get returns None if the selected item is not present
+        result = receive_menu(menu)
+        if not result:
+            print('Invalid input')
+    # Act on the action ('Menu Text',function_name)
+    print(result[1])
+
+#
+#   MAIN MENU
+#
+
+# Functions
+def tidal_whats_new():
+    pass
+def tidal_playlists():
+    pass
+def tidal_genres():
+    pass
+def user_playlists():
+    pass
+def user_albums():
+    pass
+def user_tracks():
+    pass
+def user_artists():
+    pass
+def cancel_menu():
+    pass
+def clean_exit():
+    exit()
+# Menu map
+main_menu = {'1':('Tidal What\'s New', tidal_whats_new),
+             '2':('Tidal Playlists', tidal_playlists),
+             '3':('Tidal Genres', tidal_genres),
+             '4':('Playlists', user_playlists),
+             '5':('Albums', user_albums),
+             '6':('Tracks', user_tracks),
+             '7':('Artists', user_artists),
+             '8':('Cancel',cancel_menu),
+             '9':('Quit',clean_exit)}
+
+
+
+playlists = session.get_user_playlists(session.user.id)
 
 # Add the tracks of the first user playlist to the mpv playlist
 # ! Need to slowly fetch URLs, say 3 at a time. If a playlist is 100 songs
