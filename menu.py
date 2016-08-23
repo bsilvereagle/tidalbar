b416e32223e43a1d80dd346358b9ab2084d0ec9d
bsilvereagle <bsilvereagle@gmail.com>
2016-08-23 17:41:36 -0400

    Incomplete refactoring of menu code

---
+++ menu.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+#
+#   MENU
+#
+#   Menus are dictionaries where the key is the numeric selction
+#   identifier for the menu, and the item is a tuple containing the
+#   text to be displayed and the function to be executed for the
+#   given selection
+#
+#   sw_menu = {'1':('Han',han_shot_first),'2':('Greedo',greedo_shot_first)}
+#
+#   would display the following menu when called with run_menu(sw_menu)
+#
+#   1) Han
+#   2) Greedo
+#
+#   and execute either han_shot_first() or greedo_shot_first()
+#
+#
+#   print_menu orders a menu dict by key then displays it
+#   receive_menu waits on a user selection and then returns the matching tuple
+#   run_menu calls print_menu & receive_menu until a valid menu option is chosen
+
+from collections import OrderedDict
+
+class Menu:
+    
+    def __init__(self, menu_map={}):
+        self.menu_map = menu_map 
+
+    def add_item(self, key, text, function, data=None):
+        # Adds the item to the current menu
+        # key - menu item for user to hit
+        # text - text to display in menu
+        # function - function to call on press
+        # data - any extraneous data to associate with this menu item (Track, Artist, etc)
+        
+        self.menu_map[key] = (text, function, data)
+
+    def get_item(self, key):
+        
+        return(self.menu_map[key])
+
+    def get_item_text(self, key):
+
+        return(self.menu_map[key][0])
+
+    def get_item_function(self, key):
+
+        return(self.menu_map[key][1])
+
+    def get_item_data(self, key):
+        try:
+            return(self.menu_map[key][2])
+        except:
+            return None
+
+    def print(self, end='\r\n'):
+        # Sort the menu and store in an collections.OrderedDict
+        # to ensure that items are displayed 1-9 or a-z
+        ordered_menu = OrderedDict(sorted(self.menu_map.items()))
+        
+        print('\n', end=end)
+        for item in ordered_menu:
+            print('\t%s)\t%s' % (item, ordered_menu[item][0]), end=end)
+        print('\n', end=end)
+
+
+    def run(self):
+        # Present menu and get user selection
+        result = None
+        while result is None:
+            self.print() 
+            # dict.get returns None if the selected item is not present
+            result = self.receive() 
+            if not self.get_item(result):
+                print('Invalid input', end='\r\n')
+        # Act on the action ('Menu Text',function_name)
+        result = self.get_item_function
+        result()
--- tidalbar.py
+++ tidalbar.py
@@ -6,7 +6,25 @@ import getpass
 import time
 from requests import HTTPError
 from collections import OrderedDict
+from nonblockingkb import NonBlockingKB
+from menu import Menu
 
+#
+#   KNOWN ISSUES
+#
+
+#   Pressing the arrow keys during a kb.input() cause immediate termination
+
+#
+#   EXTEND TIDALAPI
+#
+
+# All of the tidalapi classes are inherited from Model which has a .name
+# Add a __str__ function so when things are converted to strings, they
+# return their .name. Applied to Tracks/Albums/Artists/Playlists/etc
+def patch__str__(self):
+    return self.name
+setattr(tidalapi.models.Model,'__str__',patch__str__)
 
 #
 #   CREATE OBJECTS
@@ -23,6 +41,8 @@ session = tidalapi.Session()
 #   LOGIN TO TIDAL
 #
 #   Use the tidalapi to login, give the user 3 attempts before aborting
+#   
+#   * nonblockingkb is not used since getpass is more secure
 
 login_attempts = 0
 allowed_attempts = 3
@@ -45,61 +65,36 @@ if login_attempts == allowed_attempts:
     print('Failed to login after three attempts. Aborting.')
     exit()
 
+# Kick on the nonblocking keyboard now that getpass() has been used 
+kb = NonBlockingKB()
+
 #
 #   MENUS
 #
 #   Menus are dictionaries where the key is the numeric selction
 #   identifier for the menu, and the item is a tuple containing the
 #   text to be displayed and the function to be executed for the
-#   given selection
+#   given selection and any data to be tagged with the item
 #
-#   sw_menu = {'1':('Han',han_shot_first),'2':('Greedo',greedo_shot_first)}
+#   sw_menu = {'1':('Han',han_shot_first,mf),'2':('Greedo',greedo_shot_first)}
 #
 #   would display the following menu when called with run_menu(sw_menu)
 #
 #   1) Han
 #   2) Greedo
 #
-#   and execute either han_shot_first() or greedo_shot_first()
-#
-#   
-
-#
-#   MENU FUNCTIONS
+#   and execute either han_shot_first(mf) or greedo_shot_first()
 #
 
-def print_menu(menu_map):
-    # Sort the menu and store in an collections.OrderedDict
-    # to ensure that items are displayed 1-9
-    ordered_menu = OrderedDict(sorted(menu_map.items()))
-    
-    print('\n')
-    for item in ordered_menu:
-        print('\t%s)\t%s' % (item, ordered_menu[item][0]))
-    print('\n')
-
-def receive_menu(menu_map):
-    selection = input('Selection: ')
-    # Look up the selection in the dictionary
-    return(menu_map.get(selection))
-
-def run_menu(menu):
-    # Present menu and get user selection
-    result = None
-    while result is None:
-        print_menu(menu) 
-        # dict.get returns None if the selected item is not present
-        result = receive_menu(menu)
-        if not result:
-            print('Invalid input')
-    # Act on the action ('Menu Text',function_name)
-    print(result[1])
 
 #
 #   MAIN MENU
 #
 
-# Functions
+def play_playlist(playlist_id):
+    pass
+
+#  Main Menu Functions
 def tidal_whats_new():
     pass
 def tidal_playlists():
@@ -107,7 +102,14 @@ def tidal_playlists():
 def tidal_genres():
     pass
 def user_playlists():
-    pass
+    playlist_menu = Menu()
+
+    for counter, playlist in enumerate(session.get_user_playlists(session.user.id)):
+        playlist_menu.add_item(str(counter),playlist.name,play_playlist,playlist)
+    playlist_menu.print() 
+
+    selection = kb.input('Menu Selection: ')
+
 def user_albums():
     pass
 def user_tracks():
@@ -116,35 +118,62 @@ def user_artists():
     pass
 def cancel_menu():
     pass
+
 def clean_exit():
+    kb.reset()
     exit()
-# Menu map
-main_menu = {'1':('Tidal What\'s New', tidal_whats_new),
-             '2':('Tidal Playlists', tidal_playlists),
-             '3':('Tidal Genres', tidal_genres),
-             '4':('Playlists', user_playlists),
-             '5':('Albums', user_albums),
-             '6':('Tracks', user_tracks),
-             '7':('Artists', user_artists),
-             '8':('Cancel',cancel_menu),
-             '9':('Quit',clean_exit)}
+
+main_menu = Menu({'1':('Tidal What\'s New', tidal_whats_new),
+                  '2':('Tidal Playlists', tidal_playlists),
+                  '3':('Tidal Genres', tidal_genres),
+                  '4':('Playlists', user_playlists),
+                  '5':('Albums', user_albums),
+                  '6':('Tracks', user_tracks),
+                  '7':('Artists', user_artists),
+                  '8':('Cancel',cancel_menu),
+                  '9':('Quit',clean_exit)})
+
+#
+#   MAIN LOOP
+#
+
+try:
+    # Run the main menu to start
+    selection = None
+    while selection is None:
+        main_menu.print()
+        selection = kb.input('Menu Selection: ')
+        if not main_menu.get_item(selection):
+            print('Invalid Selection', end='\r\n')
+            
+    data = main_menu.get_item_data(selection)
+    func = main_menu.get_item_function(selection)
+    if data:
+        func(data)
+    else:
+        func()
+
+    while True:
+        # Check for hotkeys keyboard input
+        keypress = kb.getch()
+        if keypress != -1:
+            result = True 
+            if result:
+                # Call the resulting function
+                pass
+            
+        # Move songs from the internal queue to the mpv queue
+        # If no songs are left in the internal queue, start song radio
+                
 
 
+        # Get durations and what not
+        total_m, total_s = [round(time) for time in divmod(player.duration)]  
+        current_m, current_s = [round(time) for time in divmod(player.playback_time)]
 
-playlists = session.get_user_playlists(session.user.id)
-
-# Add the tracks of the first user playlist to the mpv playlist
-# ! Need to slowly fetch URLs, say 3 at a time. If a playlist is 100 songs
-#   long the URL may have expired
-if len( playlists ) > 0:
-    for track in session.get_playlist_tracks(playlists[0].id):
-        player.command('loadfile','rtmp://'+session.get_media_url(track.id),'append-play')
-
-    # Print the number of tracks in the playlist
-    print('%s tracks added to the mpv playlist' % len(player.playlist))
-
-    while(len(player.playlist) != 0):
-        time.sleep(60)
-else:
-    print('No playlists found')
-
+        # Print play time
+        print('%i:%i/%i:%i' % (current_m, current_s, total_m, total_s), end='\r')
+        time.sleep(1)
+finally:
+    # Reset the terminal to a nice state
+    clean_exit()
